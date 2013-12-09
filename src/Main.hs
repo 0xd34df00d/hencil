88eacf20185d339f99fb74ebfca5a3ef7c42a825
0xd34df00d <0xd34df00d@gmail.com>
2013-12-09 21:41:18 +0400

    Take full horizontal force into account.

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,7 @@ import Data.List
 import Debug.Trace
 
 μ = 1.0
-δt = 0.01
+δt = 0.001
 g = 1
 l = 1
 m = 1
@@ -17,25 +17,31 @@ data PenState = PenState {
 data Forces = Forces {
             φ'' :: Double,
             fN :: Double,
+            fH :: Double,
             fF :: Double
         } deriving (Show)
 
 initState φ = PenState 0 φ 0 0
 
 stepState :: PenState -> PenState
-stepState ps@(PenState xPos φ v ω) = let ps = PenState (xPos + δx) (φ + δφ) (v + δv) (ω + δω) in ps `traceShow` ps
-    where fs@(Forces φ'' fN fF) = calcForces ps
+stepState ps@(PenState xPos φ v ω) = let ps = PenState (xPos + δx) (φ + δφ) (v + δv) (ω + δω) in (ps, fs) `traceShow` ps
+    where fs@(Forces φ'' fN fH fF) = calcForces ps
           δω = φ'' * δt
           δφ = ω * δt + δω * δt / 2
-          δv = fF / m
-          δx = {-v * δt +-} δv * δt / 2
+          δv = δt * (fH - fF) / m
+          δx = v * δt + δv * δt / 2
 
 calcForces :: PenState -> Forces
-calcForces (PenState _ φ _ _) = Forces φ'' fN fF
-    where φ'' = 3 * g * sin φ / l ^ 2
-          ac = φ'' * l / 2
-          fN = m * g - m * ac * sin φ
-          fF = max 0 $ m * ac * cos φ - μ * (m * g - m * ac * sin φ)
+calcForces (PenState _ φ v ω) = Forces φ'' fN fH fF
+    where φ'' = 3 * g * sin φ / (2 * l)
+          aτ = φ'' * l
+          an = ω ^ 2 * l
+          av = aτ * sin φ + an * cos φ
+          ah = aτ * cos φ - an * sin φ
+          fN = m * g - m * av
+          fH = m * ah
+          fF | abs fH <= μ * fN = fH
+             | otherwise = signum fH * μ * fN 
 
 whole = unfoldr f
     where f st | φ st' < pi / 2 = Just (st', st')
