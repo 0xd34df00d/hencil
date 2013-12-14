32c8deeeed4026d3548059f6359c7f8c95480ae7
0xd34df00d <0xd34df00d@gmail.com>
2013-12-14 12:56:40 +0400

    File serializers.

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE ViewPatterns #-}
+
 import Data.List
+import Control.Arrow
 import Debug.Trace
 
 δt = 0.001
-g = 1
+g = 10
 l = 1
 m = 1
 
@@ -23,8 +26,8 @@ data Forces = Forces {
 
 initState μ φ = PenState μ 0 φ 0 0
 
-stepState :: PenState -> PenState
-stepState ps@(PenState μ xPos φ v ω) = let ps = PenState μ (xPos + δx) (φ + δφ) (v + δv) (ω + δω) in (ps, fs) `traceShow` ps
+stepState :: PenState -> (PenState, Forces)
+stepState ps@(PenState μ xPos φ v ω) = let ps = PenState μ (xPos + δx) (φ + δφ) (v + δv) (ω + δω) in {-(ps, fs) `traceShow`-} (ps, fs)
     where fs@(Forces φ'' fN fH fF) = calcForces ps
           δω = φ'' * δt
           δφ = ω * δt + δω * δt / 2
@@ -44,12 +47,16 @@ calcForces (PenState μ _ φ v ω) = Forces φ'' fN fH fF
              | otherwise = signum fH * μ * fN 
 
 whole = unfoldr f
-    where f st | φ st' < pi / 2 = Just (st', st')
+    where f st | φ st' < pi / 2 = Just ((st', fs, (xPos st' - xPos st) / δt), st')
                | otherwise = Nothing
-            where st' = stepState st
+            where (st', fs) = stepState st
 
-toFile :: [PenState] -> String
-toFile = intercalate "\n" . map (\(PenState _ x φ _ _) -> show (x - l * sin φ) ++ " " ++ show (l * cos φ))
+toFile :: [(PenState, Forces, Double)] -> String
+toFile = intercalate "\n" . map (\(PenState _ x φ _ _, _, _) -> show (x - l * sin φ) ++ " " ++ show (l * cos φ))
 
-toFileEnd :: [PenState] -> String
-toFileEnd = intercalate "\n" . map (\(t, (PenState _ x φ _ _)) -> show t ++ " " ++ show x) . zip [1..]
+toFileEnd :: [(PenState, Forces, Double)] -> String
+toFileEnd = intercalate "\n" . map (\(t, (PenState _ x φ _ _, Forces _ fN _ fF, v)) -> show (t * δt) ++ " " ++ show x ++ " " ++ show (-fN / g) ++ " " ++ show (-fF / g) ++ " " ++ show v) . zip [1.0,2.0..]
+
+makeEndPoss :: [Double] -> [(Double, Double)]
+makeEndPoss = map $ id &&& (\(last . whole . (`initState` 0.00001) -> (PenState _ xPos _ _ _, _, _)) -> xPos)
+
